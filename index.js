307356ca0413d252266abefa80febc38d6dd7bf4
Sergiy Pavlenko <onmyouza.serg@gmail.com>
2019-07-02 15:47:25 +0300

    getting data from api

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', async function (req, res) {
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/currency', async function (req, res) {
     var data = currencyRequest();
-    console.log(data);
+    //console.log(data);
     res.render('index');
 });
    
--- requests/currency.request.js
+++ requests/currency.request.js
@@ -1,13 +1,35 @@
 const rp = require('request-promise');
 
 module.exports = async function() {
-    const uri = 'https://7np770qqk5.execute-api.eu-west-1.amazonaws.com/prod/get-transaction';
+    const originalTansaction = 'https://7np770qqk5.execute-api.eu-west-1.amazonaws.com/prod/get-transaction';
     
     var options = {
-        uri,
+        uri: originalTansaction,
         json: true
     };
 
-    const data = await(rp(options));
-    console.log(data);
+    const rates = 'https://api.exchangeratesapi.io/latest';
+
+    var opt = {
+        uri: rates,
+        json: true,
+    };
+
+    let transaction = await(rp(options));
+    let amount = transaction.amount;
+    let currency = transaction.currency;
+    let curRates = await(rp(opt));
+    transaction.convertedAmount = convertedAmount();
+    
+    function convertedAmount(){
+        let result = (amount / curRates.rates[currency]);
+        if(result % 1 !== 0) {
+            return +result.toFixed(4);
+        }
+        
+    }
+
+    console.log(transaction);
+
+    
 };
